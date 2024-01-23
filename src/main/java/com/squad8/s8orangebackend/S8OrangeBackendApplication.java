064bd5d6ca937731e851ad6dfca437e3cf2fce09
Thimachal <thimachal@gmail.com>
2024-01-23 12:07:57 -0300

    creation backend project

---
+++ src/main/java/com/squad8/s8orangebackend/S8OrangeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.squad8.s8orangebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S8OrangeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S8OrangeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squad8/s8orangebackend/S8OrangeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad8.s8orangebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S8OrangeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
