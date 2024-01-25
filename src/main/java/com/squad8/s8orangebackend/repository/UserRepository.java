c1d92f9a69c4cbaa79880792ffbe3028273e5c59
EricklisCruz <erickliscruz@gmail.com>
2024-01-25 12:59:17 -0300

    CREATED USER REGISTRATION DTO, USER REPOSITORY

---
+++ src/main/java/com/squad8/s8orangebackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.squad8.s8orangebackend.repository;
+
+import com.squad8.s8orangebackend.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
