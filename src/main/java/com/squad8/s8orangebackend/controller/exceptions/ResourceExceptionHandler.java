0c88b199bd7e356e9815d84b5cbbfe494d41bb6f
EricklisCruz <erickliscruz@gmail.com>
2024-01-25 16:43:44 -0300

    CLASS RESOURCE EXCEPTION HANDLER

---
+++ src/main/java/com/squad8/s8orangebackend/controller/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.squad8.s8orangebackend.controller.exceptions;
+
+import com.squad8.s8orangebackend.service.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error,
+                e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+}
