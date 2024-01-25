c0a7755e1868c6c31011318ad8ed8f7427b4b6fe
EricklisCruz <erickliscruz@gmail.com>
2024-01-25 20:03:02 -0300

    ENDPOINT /USER/REGISTER MODIFIED

--- src/main/java/com/squad8/s8orangebackend/service/UserService.java
+++ src/main/java/com/squad8/s8orangebackend/service/UserService.java
@@ -1,6 +1,7 @@
 package com.squad8.s8orangebackend.service;
 
 import com.squad8.s8orangebackend.domain.user.User;
+import com.squad8.s8orangebackend.dtos.UserRegistrationDto;
 import com.squad8.s8orangebackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,11 @@ public class UserService {
 
     public User insertUser(User user) {
         return userRepository.save(user);
+    }
+
+    public User fromDto(UserRegistrationDto userRegistrationDto) {
+        return new User(userRegistrationDto.getName(),
+                userRegistrationDto.getSurname(), userRegistrationDto.getEmail(),
+                userRegistrationDto.getPassword());
     }
 }
