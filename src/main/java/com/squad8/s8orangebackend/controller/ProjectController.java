2608d4e88f51b9cb82e3fd06893150b6d5820b5c
Thimachal <thimachal@gmail.com>
2024-01-25 22:29:40 -0300

    revised endpoint Get entity Project

--- src/main/java/com/squad8/s8orangebackend/controller/ProjectController.java
+++ src/main/java/com/squad8/s8orangebackend/controller/ProjectController.java
@@ -1,25 +1,21 @@
 package com.squad8.s8orangebackend.controller;
-
+import com.squad8.s8orangebackend.domain.project.Project;
 import com.squad8.s8orangebackend.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/project")
 public class ProjectController {
     @Autowired
     private ProjectService actionProjectService;
-
-    @GetMapping("/listprojects")
-    public ResponseEntity<?> listProjects() {
-        try {
-            return new ResponseEntity<>(actionProjectService.listProjects().getBody(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+    @GetMapping("/list")
+    public ResponseEntity<List<Project>> listProjects() {
+        List<Project> projects = actionProjectService.listProjects();
+        return ResponseEntity.ok().body(projects);
     }
 }
--- src/main/java/com/squad8/s8orangebackend/service/ProjectService.java
+++ src/main/java/com/squad8/s8orangebackend/service/ProjectService.java
@@ -1,17 +1,15 @@
 package com.squad8.s8orangebackend.service;
 
+import com.squad8.s8orangebackend.domain.project.Project;
 import com.squad8.s8orangebackend.repository.ProjectRepository;
-import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 @Service
 public class ProjectService {
     @Autowired
     private ProjectRepository actionProjectRepository;
-    public ResponseEntity<?> listProjects() {
-        return ResponseEntity.ok(actionProjectRepository.findAll());
+    public List<Project> listProjects() {
+        return actionProjectRepository.findAll();
     }
 }
