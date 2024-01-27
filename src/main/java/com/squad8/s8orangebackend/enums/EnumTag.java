d832bd9fa78c4949a7d1ff880b8953b22c8bef70
EricklisCruz <erickliscruz@gmail.com>
2024-01-27 14:14:49 -0300

    CREATED USER ROLE, MIGRATION AND PACKAGE ENUMS

--- src/main/java/com/squad8/s8orangebackend/enums/EnumTag.java
+++ src/main/java/com/squad8/s8orangebackend/enums/EnumTag.java
@@ -1,4 +1,4 @@
-package com.squad8.s8orangebackend.domain.project;
+package com.squad8.s8orangebackend.enums;
 
 public enum EnumTag {
     CSharp,CSS,HTML,Java,JavaScript,MySQL,PHP,PostgreSQL,Spring
