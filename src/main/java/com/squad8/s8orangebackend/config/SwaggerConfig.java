8123781520d672f5a59a43cb36499b96275bcc19
EricklisCruz <erickliscruz@gmail.com>
2024-01-27 17:48:13 -0300

    ALTERED SWAGGER CONFIGURATION

---
+++ src/main/java/com/squad8/s8orangebackend/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.squad8.s8orangebackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("API ORANGE FCAMARA HACKATHON")
+                        .description("API REST")
+                        .version("1.0").contact(new Contact().name("SQ8"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
